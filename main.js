74381d72ac44942a1ed51cbe8a88c81096779f39
Niicklz <77895883+Niicklz@users.noreply.github.com>
2023-10-12 16:24:36 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+document.getElementById("open").addEventListener("click",()=> {
+    document.getElementById("nav").classList.toggle("active")
+} )
+
+document.getElementById("close").addEventListener("click",()=> {
+    document.getElementById("nav").classList.toggle("active")
+} )
